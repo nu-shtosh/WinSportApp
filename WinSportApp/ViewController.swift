8b003df7787c7a94d39dc2668d4d5895496552f9
ILIA <dubenskiiilia@gmail.com>
2023-07-13 14:23:16 +0300

    Initial Commit

---
+++ WinSportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WinSportApp
+//
+//  Created by Илья Дубенский on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
